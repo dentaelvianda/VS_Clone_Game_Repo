d43cb62df2707795899069df7bfa26986f5bca4e
ilmaralamsyah <ilmaralamsyah@gmail.com>
2024-06-03 22:58:08 +0700

    zombie dll

--- Vampire Survivor Clone Game Project/Assets/Script/EnemyAI.cs
+++ Vampire Survivor Clone Game Project/Assets/Script/EnemyAI.cs
@@ -9,11 +9,19 @@ public class EnemyAI : MonoBehaviour
 
     private Transform target;
     private NavMeshAgent navMeshAgent;
-    private bool chaseTarget = false;
+    private bool chaseTarget;
+    private bool isEnemyMoving;
+    private Vector3 previousPosition;
+    private SpriteRenderer spriteRenderer;
 
     private void Awake()
     {
         target = FindObjectOfType<Player>().transform;
+        previousPosition = transform.position;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        isEnemyMoving = false;
+        chaseTarget = false;
     }
 
     private void Start()
@@ -21,6 +29,8 @@ public class EnemyAI : MonoBehaviour
         navMeshAgent = GetComponent<NavMeshAgent>();
         navMeshAgent.updateRotation = false;
         navMeshAgent.updateUpAxis = false;
+
+        
     }
 
     private void Update()
@@ -30,13 +40,39 @@ public class EnemyAI : MonoBehaviour
             if (Vector3.Distance(transform.position, target.position) < minEnemyTargetDistance && chaseTarget)
             {
                 navMeshAgent.SetDestination(target.position);
+                isEnemyMoving = true;
+            }
+            else
+            {
+                isEnemyMoving = false;
             }
         }
-        
+
+        Vector3 currentPosition = transform.position;
+        Vector3 movementDirection = currentPosition - previousPosition;
+
+        // Cek arah gerakan musuh di sumbu X
+        if (movementDirection.x < 0)
+        {
+            // Musuh bergerak ke kiri (flip sprite)
+            spriteRenderer.flipX = true;
+        }
+        else if (movementDirection.x > 0)
+        {
+            // Musuh bergerak ke kanan (default)
+            spriteRenderer.flipX = false;
+        }
+
+        previousPosition = currentPosition;
     }
 
     public void SetChaseTarget(bool chaseTarget)
     {
         this.chaseTarget = chaseTarget;
+    }
+
+    public bool IsEnemyMoving()
+    {
+        return isEnemyMoving;
     }
 }
