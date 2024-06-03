5c32e918c497bf91e680ca628963b1e24f43db2b
ilmaralamsyah <ilmaralamsyah@gmail.com>
2024-06-04 00:08:44 +0700

    Spawner

--- Vampire Survivor Clone Game Project/Assets/Script/EnemySpawner.cs
+++ Vampire Survivor Clone Game Project/Assets/Script/EnemySpawner.cs
@@ -6,8 +6,33 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private List<Transform> spawnPositionList;
     [SerializeField] private GameObject[] enemyPrefabs;
+    [SerializeField] private float waveTimer = 5f;
+    [SerializeField] private int totalWave;
+    [SerializeField] private bool usingWave;
 
+    private float waveCounter;
+    private int currentWave;
+ 
     private bool IsSpawned = false;
+
+    private void Update()
+    {
+        if (usingWave && currentWave < totalWave)
+        {
+            
+            waveCounter += Time.deltaTime;
+            if(waveCounter > waveTimer)
+            {
+                foreach (Transform enemyTransform in spawnPositionList)
+                {
+                    GameObject enemySpawned = Instantiate(enemyPrefabs[GetRandomNumber()], enemyTransform);
+                    enemySpawned.GetComponent<EnemyAI>().SetChaseTarget(true);
+                }
+                currentWave++;
+            }
+            
+        }
+    }
 
     public void SpawnEnemy()
     {
@@ -17,6 +42,7 @@ public class EnemySpawner : MonoBehaviour
             GameObject enemySpawned = Instantiate(enemyPrefabs[GetRandomNumber()], enemyTransform);
             enemySpawned.GetComponent<EnemyAI>().SetChaseTarget(true);
         }
+        waveCounter = 0f;
         IsSpawned = true;
     }
 
